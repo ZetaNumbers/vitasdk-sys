d91c500a628f07c169bb6131f1326c45d904a310
Daria Sukhonina <dariasukhonina@gmail.com>
2023-10-16 14:54:39 +0300

    feat(build-util)!: Switch to argh for build-util
    Changes feature names
    Bumps build-util version

--- build-util/src/lib.rs
+++ build-util/src/lib.rs
@@ -1,3 +1,6 @@
-#[cfg(feature = "link-visitor")]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
+#[cfg(feature = "build-dep")]
+#[cfg_attr(docsrs, doc(cfg(feature = "build-dep")))]
 pub mod link_visitor;
 pub mod vita_headers_db;
--- build-util/src/main.rs
+++ build-util/src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{BTreeSet, HashSet},
+    collections::BTreeSet,
     env,
     path::PathBuf,
     process::{self, Command, ExitCode},
@@ -30,137 +30,82 @@ fn vita_headers_db_path() -> PathBuf {
         .join("vita-headers/db")
 }
 
-fn print_help() {
-    eprintln!(
-        "\
-Internal build utilities for vitasdk-sys crate
-
-USAGE:
-    vitasdk-sys-build-util [OPTIONS] <COMMAND>
-
-Commands:
-    stub-libs   Print all stub lib names
-    help        Print help
-
-Options
-    -h, --help  Print help
-"
-    )
-}
-
 fn main() -> ExitCode {
     env_logger::init();
 
-    let cmd = std::env::args().nth(1);
-    match cmd.as_deref() {
-        Some("stub-libs") => stub_libs(),
-        Some("help" | "--help" | "-h") => {
-            print_help();
-            ExitCode::SUCCESS
-        }
-        _ => {
-            print_help();
-            ExitCode::FAILURE
-        }
+    let args: TopLevelArgs = argh::from_env();
+    match &args.subcommand {
+        Subcommands::StubLibs(a) => stub_libs(a),
     }
 }
 
-fn stub_libs() -> ExitCode {
-    fn print_help() {
-        eprintln!(
-            "\
-Print stub lib names
+#[derive(argh::FromArgs, PartialEq, Debug)]
+/// Internal build utilities for vitasdk-sys crate
+struct TopLevelArgs {
+    #[argh(subcommand)]
+    subcommand: Subcommands,
+}
 
-USAGE:
-    vitasdk-sys-build-util stub-libs [OPTIONS]
+#[derive(argh::FromArgs, PartialEq, Debug)]
+#[argh(subcommand)]
+enum Subcommands {
+    StubLibs(SubCommandStubLibs),
+}
 
-Options:
-    -h, --help          Print help
-    -u, --user          Print only user stub libs. Mutually exclusive with `--kernel`.
-    -k, --kernel        Print only kernel stub libs. Mutually exclusive with `--user`.
-    -c, --conflicting   Print only stub libs with conflicting symbols
-    --with-conflicting  Include stub libs with conflicting symbols
-    --missing-features  Print only undefined vitasdk-sys features
-    --as-features       Print stub libs as cargo features
-    --missing-libs      Print only stub libs which do not exist in `$VITASDK/arm-vita-eabi/lib`
-    --fail-if-any       Fail if any stub lib is printed
-"
-        )
-    }
+#[derive(argh::FromArgs, PartialEq, Debug)]
+#[argh(subcommand, name = "stub-libs")]
+/// Print stub lib names
+struct SubCommandStubLibs {
+    /// print only user stub libs, mutually exclusive with `--kernel`
+    #[argh(switch, short = 'u')]
+    user: bool,
+    /// print only kernel stub libs, mutually exclusive with `--user`
+    #[argh(switch, short = 'k')]
+    kernel: bool,
+    /// print only stub libs with conflicting symbols
+    #[argh(switch, short = 'c')]
+    conflicting: bool,
+    /// include stub libs with conflicting symbols
+    #[argh(switch)]
+    with_conflicting: bool,
+    /// print only undefined vitasdk-sys features
+    #[argh(switch)]
+    missing_features: bool,
+    /// print stub libs as cargo features
+    #[argh(switch)]
+    as_features: bool,
+    /// print only stub libs which do not exist in `$VITASDK/arm-vita-eabi/lib`
+    #[argh(switch)]
+    missing_libs: bool,
+    /// fail if any stub lib is printed
+    #[argh(switch)]
+    fail_if_any: bool,
+}
 
-    #[derive(Clone, Copy, PartialEq, Eq, Hash)]
-    enum Flag {
-        Help,
-        User,
-        Kernel,
-        Conflicting,
-        WithConflicting,
-        MissingFeatures,
-        AsFeatures,
-        MissingLibs,
-        FailIfAny,
-    }
-
-    #[derive(Debug)]
-    struct ParseFlagError;
-
-    impl std::str::FromStr for Flag {
-        type Err = ParseFlagError;
-
-        fn from_str(s: &str) -> Result<Self, Self::Err> {
-            match s {
-                "-h" | "--help" => Ok(Flag::Help),
-                "-u" | "--user" => Ok(Flag::User),
-                "-k" | "--kernel" => Ok(Flag::Kernel),
-                "-c" | "--conflicting" => Ok(Flag::Conflicting),
-                "--with-conflicting" => Ok(Flag::WithConflicting),
-                "--missing-features" => Ok(Flag::MissingFeatures),
-                "--as-features" => Ok(Flag::AsFeatures),
-                "--missing-libs" => Ok(Flag::MissingLibs),
-                "--fail-if-any" => Ok(Flag::FailIfAny),
-                _ => Err(ParseFlagError),
-            }
-        }
-    }
-
-    let Some(options) = std::env::args()
-        .skip(2)
-        .filter(|a| !a.is_empty())
-        .map(|op| op.parse())
-        .collect::<Result<HashSet<Flag>, ParseFlagError>>()
-        .ok()
-        .filter(|ops| !ops.contains(&Flag::Kernel) || !ops.contains(&Flag::User))
-    else {
-        print_help();
-        return ExitCode::FAILURE;
-    };
-
-    if options.contains(&Flag::Help) {
-        print_help();
-        return ExitCode::SUCCESS;
-    }
-
+fn stub_libs(args: &SubCommandStubLibs) -> ExitCode {
     let mut db = VitaDb::load(&vita_headers_db_path());
-    if options.contains(&Flag::Conflicting) {
+    if args.conflicting {
         db = db.split_conflicting();
-    } else if !options.contains(&Flag::WithConflicting) {
+    } else if !args.with_conflicting {
         db.split_conflicting();
     }
 
-    if options.contains(&Flag::User) {
+    assert!(
+        !(args.user & args.kernel),
+        "`--kernel` and `--user` cli switches are mutually exclusive"
+    );
+    if args.user {
         db.split_kernel();
-    } else if options.contains(&Flag::Kernel) {
+    } else if args.kernel {
         db = db.split_kernel();
     }
 
     let vitasdk_sys_manifest = vitasdk_sys_manifest();
     let stub_libs: BTreeSet<_> = {
-        let mut missing_features_filter = options
-            .contains(&Flag::MissingFeatures)
+        let mut missing_features_filter = args
+            .missing_features
             .then(|| missing_features_filter(&vitasdk_sys_manifest));
-        let mut missing_libs_filter = options
-            .contains(&Flag::MissingLibs)
-            .then(missing_libs_filter);
+        let mut missing_libs_filter = args.missing_libs.then(missing_libs_filter);
 
         db.stub_lib_names()
             .filter(|s| {
@@ -173,7 +118,7 @@ Options:
             .collect()
     };
 
-    if options.contains(&Flag::AsFeatures) {
+    if args.as_features {
         stub_libs
             .iter()
             .for_each(|stub_lib| println!("{stub_lib} = []"));
@@ -181,7 +126,7 @@ Options:
         stub_libs.iter().for_each(|stub_lib| println!("{stub_lib}"));
     }
 
-    if !stub_libs.is_empty() && options.contains(&Flag::FailIfAny) {
+    if !stub_libs.is_empty() && args.fail_if_any {
         return ExitCode::FAILURE;
     }
 
